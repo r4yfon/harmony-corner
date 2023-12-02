ed968147e8a3220da5f52c7f84f229959e3ffb29
r4yfon <75479544+r4yfon@users.noreply.github.com>
2023-12-03 02:31:21 +0800

    basic navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          <icon-solar-square-academic-cap-bold height="48" width="48" />
+        </a>
+
+        <!-- the button that will appear on mobile viewports -->
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- actual navbar in desktop viewport -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <select class="form-select bg-body-tertiary border-0" v-model="selected_school">
+              <option selected value="模范班">模范班</option>
+              <option value="青少年班">青少年班</option>
+            </select>
+            <div class="vr mx-3 d-none d-lg-block"></div>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Classes</a>
+            </li>
+          </ul>
+
+          <div class="nav-item dropdown">
+            <a
+              class="nav-link p-2"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <icon-solar-user-circle-linear width="24" height="24" />
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">View account</a></li>
+              <li><a class="dropdown-item" href="#">Edit details</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item text-danger" href="#">Log out</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+nav {
+  box-shadow: 0 3px 12px #00000014;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      selected_school: "模范班"
+    };
+  }
+};
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
+import Components from "unplugin-vue-components/vite";
+import IconsResolver from "unplugin-icons/resolver";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [
+        IconsResolver({
+          prefix: "icon"
+        })
+      ]
+    }),
+    Icons({
+      compiler: "vue3",
+      autoInstall: true
+    })
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   }
-})
+});
